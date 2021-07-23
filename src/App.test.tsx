ab0911da81541939d392fd04bb632a60a183a48a
Parth Makadiya <parth.makadiya@hexad.de>
2021-07-23 18:38:50 +0200

    add enzyme and update App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App/>', () => {
+  it('should have hello world text', () => {
+    const wrapper=shallow(<App/>)
+    const text=wrapper.find("div").text();
+    expect(text).toBe("Hello World")
+  });
 });
