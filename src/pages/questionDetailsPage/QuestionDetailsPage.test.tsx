73a00a099fe8b41ca4cd708b04f933d2fa55eb17
parthmakadiya12 <parthmakadiya12@gmail.com>
2021-08-01 23:27:34 +0200

    create question details page

--- src/pages/questionDetailsPage/QuestionDetailsPage.test.tsx
+++ src/pages/questionDetailsPage/QuestionDetailsPage.test.tsx
@@ -1,9 +1,90 @@
-import QuestionDetailsPage from "./QuestionDetailsPage";
+import renderer from "react-test-renderer";
+import QuestionDetailsPage, { PropTypes } from "./QuestionDetailsPage";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "../../styles/theme";
+import "jest-styled-components";
 
-describe('<QuestionsDetailsPage>', () => {
-  it('should have correct text rendered', () => {
-    const wrapper = shallow(<QuestionDetailsPage />);
-    const text = wrapper.find("div").text();
-    expect(text).toBe("QuestionDetailsPage");
+describe("<QuestionsDetailsPage>", () => {
+  let props: PropTypes;
+  beforeEach(() => {
+    props = {
+      question: {
+        choices: [
+          {
+            choice: "Swift",
+            percentage: 53.33,
+            url: "/questions/1/choices/1",
+            votes: 2048,
+          },
+          {
+            choice: "Python",
+            percentage: 26.67,
+            url: "/questions/1/choices/2",
+            votes: 1024,
+          },
+          {
+            choice: "Objective-C",
+            percentage: 13.33,
+            url: "/questions/1/choices/3",
+            votes: 512,
+          },
+          {
+            choice: "Ruby",
+            percentage: 6.67,
+            url: "/questions/1/choices/4",
+            votes: 256,
+          },
+        ],
+        published_at: "2014-11-11T08:40:51.620Z",
+        question: "Favourite programming language?",
+        url: "/questions/1",
+      },
+      getQuestionData: jest.fn(),
+      voteOnQuestion: jest.fn(),
+    };
+  });
+  it("should render correctly", () => {
+    const wrapper = mount(
+      <ThemeProvider theme={darkTheme}>
+        <QuestionDetailsPage {...props} />
+      </ThemeProvider>
+    ).debug();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should not call voteOnQuestion if none of the radio is selected", () => {
+    const wrapper = mount(
+      <ThemeProvider theme={darkTheme}>
+        <QuestionDetailsPage {...props} />
+      </ThemeProvider>
+    );
+    wrapper.find("button").simulate("click");
+    expect(props.voteOnQuestion).not.toHaveBeenCalled();
+  });
+
+  it("should call voteOnQuestion by clicking vote button", () => {
+    const wrapper = mount(
+      <ThemeProvider theme={darkTheme}>
+        <QuestionDetailsPage {...props} />
+      </ThemeProvider>
+    );
+    wrapper.find("span").at(1).simulate("click");
+    wrapper.find("button").simulate("click");
+    expect(props.voteOnQuestion).toHaveBeenCalledWith(
+      "/questions/1/choices/1",
+      "/questions/1"
+    );
+  });
+
+  it("should call getQuestionData if user visits child page directly", () => {
+    window.location = new URL("https://www.example.com/questions/123");
+    const newProps = { ...props, question: undefined };
+    const wrapper = mount(
+      <ThemeProvider theme={darkTheme}>
+        <QuestionDetailsPage {...newProps} />
+      </ThemeProvider>
+    );
+
+    expect(props.getQuestionData).toHaveBeenCalled();
   });
 });
--- src/pages/questionDetailsPage/QuestionDetailsPage.tsx
+++ src/pages/questionDetailsPage/QuestionDetailsPage.tsx
@@ -1,7 +1,76 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+import styled from "styled-components";
 
-const QuestionDetailsPage: FC = () => {
-  return <div id="question-details">QuestionDetailsPage</div>;
+import { CardTitle, Wrapper, Row } from "../../common/styles/layout";
+import { P } from "../../common/styles/typography";
+import { Radio } from "../../common/Radio/Radio";
+import { Button } from "../../common";
+import { QuestionType } from "../../common/types/QuestionType";
+
+export interface PropTypes {
+  question?: QuestionType;
+  getQuestionData: (id: any) => void;
+  voteOnQuestion: (name: string, url: string) => void;
+}
+const QuestionDetailsPage: FC<PropTypes> = ({
+  question,
+  voteOnQuestion,
+  getQuestionData,
+}) => {
+  const [name, setName] = useState<string>("");
+  const [choiceUrl, setChoiceUrl] = useState<string>("");
+  const [quesUrl, setQuesUrl] = useState<string>("");
+
+  const clickHandler = (name: string, url: string, queUrl: string) => {
+    setName(name);
+    setChoiceUrl(url);
+    setQuesUrl(queUrl);
+  };
+
+  const handleSave = (quesUrl: string, choiceUrl: string) => {
+    if (!!quesUrl && !!choiceUrl) {
+      voteOnQuestion(quesUrl, choiceUrl);
+    }
+  };
+
+  useEffect(() => {
+    if (!question) {
+      const currentUrl = window.location.pathname;
+      const questionIdRegex = new RegExp(`[^\/questions\/][0-9]*$`, "g");
+      const questionId = currentUrl.match(questionIdRegex);
+      getQuestionData(questionId);
+    }
+  }, []);
+
+  return (
+    <WrapperStyled>
+      <CardTitle>{question && question.question}</CardTitle>
+      {question &&
+        question.choices.map((option: any) => (
+          <Row key={`${option.url}_row`} alignItems={"center"}>
+            <Radio
+              name={option.choice}
+              choiceUrl={option.url}
+              queUrl={question.url}
+              key={`${option.url}_radio`}
+              clickHandler={(name: string, choiceUrl: string, queUrl: string) =>
+                clickHandler(name, choiceUrl, queUrl)
+              }
+              checked={false}
+              selected={name}
+            />
+            <P key={`${option.url}_text`}>{option.percentage}%</P>
+          </Row>
+        ))}
+      <Button clickHandler={() => handleSave(quesUrl, choiceUrl)}>Save</Button>
+    </WrapperStyled>
+  );
 };
+
+const WrapperStyled = styled(Wrapper)`
+  & > button {
+    margin-top: 1rem;
+  }
+`;
 
 export default QuestionDetailsPage;
